173c63dd85982cd0961f1b6e9961b3f7303fe9c2
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-20 06:34:52 -0700

    Wrote Pseudocode / Planning

---
+++ lib/pseudocode.py
@@ -0,0 +1,13 @@
+
+# The Main UI
+    # At v0.0
+
+"""
+
+    > For Now, It Will Just Maintain Temporarily
+
+    Input For To Do List With Create Icon On The Side
+    
+    ToDo Items With Complete Icon On The Side
+
+"""
